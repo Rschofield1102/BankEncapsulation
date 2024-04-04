a1e46bd5011ab795410ffad37124fb98cbf54494
Robert Schofield <rschofield1992@gmail.com>
2024-04-04 15:22:00 -0400

    commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace BankEncapsulation
+{
+	public class BankAccount
+	{
+		public BankAccount()
+		{
+		}
+		private double balance = 0; //step 1 create private field double.
+									// balance field is set to private
+
+		public void Depost(double amount)//2 create method (DEPOSIT) will accept double
+		{								 // and store it in the balance field.
+			balance += amount;
+		}
+
+		public double GetBalance() //3create Method named (GetBalance)
+		{						   // it will return the amount in the balance field
+			return balance;
+		}
+		//BELOW IS EXTRA PRACTICE
+
+		public double Withdraw(double amount)
+		{
+			
+			balance -= amount;
+			
+			return balance;
+			
+		}
+
+		
+
+
+	}
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,77 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount(); //step 4 create instance
+            Console.WriteLine("Deposit money");
+
+
+
+            var amountToDeposit = double.Parse(Console.ReadLine()); //step 6 Because step2 needed a double had to parse to double.  
+
+            account.Depost(amountToDeposit);     //step 5 deposit money
+
+            Console.WriteLine($"Your balance is now {account.GetBalance()}"); // step 7 access account, and call method to get the balance
+
+
+
+
+
+
+
+
+
+
+            //BELOW IS EXTRA PRACTICE
+
+
+
+            //Console.WriteLine("How  much would you like to withdraw?");
+
+
+            //var amountToWithdraw = double.Parse(Console.ReadLine());
+            //account.Withdraw(amountToWithdraw);
+
+            //Console.WriteLine($"Your account is now at {account.Withdraw(amountToWithdraw)}");
+
+
+
+
+
+
+
+
+
+            /////ATM
+
+
+
+            //Console.WriteLine(" ENTER Deposit, Withdraw, Balance");
+            //string userInput = (Console.ReadLine());
+
+            //if (userInput == "Withdraw") 
+
+            //{   
+            //        Console.WriteLine("How much would you like to withdraw?");
+            //        var amountToWithdraw = double.Parse(Console.ReadLine());
+            //        account.Withdraw(amountToWithdraw);
+            //        Console.WriteLine($"Your account is now at {account.GetBalance()}");
+            //}
+
+            //if (userInput == "Deposit")
+            //{
+            //    Console.WriteLine("How much would you like to deposit?");
+            //    var amountToDeposit = double.Parse(Console.ReadLine());
+            //    account.Depost(amountToDeposit);
+            //    Console.WriteLine($"Your account is now at {account.GetBalance()}");
+            //}
+
+            //else if( userInput == "Balance")
+            //{
+            //    Console.WriteLine($"{account.GetBalance()}");
+            //}
+
+
+
         }
     }
 }
